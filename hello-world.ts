5c6897032ff99ca093a7ee9b405ec5951cae625a
Diallo <diallo_2@diallo_2@etna.nx>
2024-11-29 10:47:45 +0000

    Add personalized greeting with time of day

--- hello-world.ts
+++ hello-world.ts
@@ -1,6 +1,5 @@
-// hello-world.ts
-const greet = (name: string): string => {
-    return `Hello, ${name}!`;
+const greet = (name: string, timeOfDay: string): string => {
+    return `Good ${timeOfDay}, ${name}!`;
 };
 
-console.log(greet("World"));
+console.log(greet("World", "morning"));
